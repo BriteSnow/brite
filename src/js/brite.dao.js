a752e3e79bed3ca5822bc56cf5293ecd1c2889fd
jeremychone <jeremy.chone@gmail.com>
2012-09-01 12:08:44 -0700

    + new brite.dao.onDataChange, onResult, onDao events

--- src/js/brite.dao.js
+++ src/js/brite.dao.js
@@ -15,6 +15,9 @@ var brite = brite || {};
  */
 brite.dao = {};
 
+// for the data events (brite.data.on() and brite.data.off())
+brite.data = {};
+
 (function($) {
 
 	var daoDic = {};
@@ -48,6 +51,372 @@ brite.dao = {};
 		return this;
 	};
 
+
+  // --------- Internal Utilities For Dao Events --------- //
+	var _ALL_ = "_ALL_";
+	
+	/**
+	 * Build the arguments for all the brite.dao.on*** events from the arguments
+	 * Can be
+	 * - (events,objectTypes,func,namespace)
+	 * - (objectTypes,func,namespace)
+	 * - (func,namspace)
+	 * and all the above without the namespace
+	 * 
+	 * Return an object with
+	 *   .events (with the namespace)
+	 *   .objectTypes (as class css selector, ".User, .Task"
+	 *   .func the function to register 
+	 *   .namespace
+	 */ 
+	function buildDaoOnEventParamMap(args){
+		var i, val, namespace, map = {};
+		
+		// build the map
+		for (i = args.length - 1; i > -1; i--){
+			val = args[i];
+			// if it is a function, set it.
+			if ($.isFunction(val)){
+				map.func = val;
+			}
+			// if we did not get the function yet, this is the name space
+			else if (!map.func){
+				namespace = val;
+		  }
+		  // if we have the func, but not the objectTypes, this is the objectTypes
+		  else if (map.func && typeof map.objectTypes === "undefined"){
+		  	map.objectTypes = val;
+		  }
+		  // if we have the func and objectTypes, then this value is the events
+		  else if (map.func &&  typeof map.objectTypes !== "undefined"){
+		  	map.events = val;
+		  }
+		}
+		
+		// create the namespace if not present
+		if (typeof namespace === "undefined"){
+			namespace = brite.uuid();
+		}
+	  
+	  // complete the event
+	  if (!map.events){
+	  	map.events = _ALL_;
+	  }else{
+	  	// build the events, split by ',', add the namespace, and join back
+		  var events = map.events.split(",");
+		  $.each(events,function(idx,val){
+		  	events[idx] = $.trim(val) + "." + namespace;
+		  });
+		  map.events = events.join(", ");
+	  }
+	  
+	  // complete the objectTypes
+	  // build the objectTypes, split by ',', add the "." prefix, and join back
+	  if (map.objectTypes){
+		  var objectTypes = map.objectTypes.split(",");
+		  $.each(objectTypes,function(idx,val){
+		  	objectTypes[idx] = "." + $.trim(val);
+		  });
+		  map.objectTypes = objectTypes.join(",");
+	  }		
+		
+		map.namespace = namespace;
+		
+		return map;
+	}
+	
+	/**
+	 * Utility method that will construct a jQuery event with the daoEvent 
+	 * and trigger it to the appropriate $receiver given the dictionary and objectType
+	 * 
+	 */
+	function _triggerDaoEvent(dic,$receiversRoot, objectType, daoEvent){
+		
+		var evt = $.extend(jQuery.Event(daoEvent.action),{daoEvent:daoEvent});
+		
+ 		var $receiver = dic[objectType];
+ 		
+ 		// if the $receiver does not exist, create it. 
+ 		if (!$receiver){
+ 			dic[objectType] = $receiver = $("<div class='" + objectType + "'></div>");
+ 			$receiversRoot.append($receiver);
+ 		}
+ 		// trigger with the event.type == action
+ 		$receiver.trigger(evt);
+ 		
+ 		// trigger _ALL_ action in case there are some events registered for all event
+ 		evt.type = _ALL_;
+ 		$receiver.trigger(evt);				
+  }
+	// --------- /Internal Utilities For Dao Events --------- //
+	
+	// --------- brite.dao.onDao --------- //
+	var $daoDao = $("<div></div>");
+	// dictionary of {objectType:$dataEventReceiver} 
+	
+	var onDaoReceiverDic = {};
+	/**
+	 * This will trigger on any DAO calls before the dao action is completed (for 
+	 * 	asynch daos), hence, the resultPromise property of the daoEvent. 
+	 * 
+	 * @param events            e.g., "create, list, get" (null for any)
+	 * @param objectTypes       e.g., "User, Task" (null for any)
+	 * @param listenerFunction  The function to be called with the daoEvent
+	 *            listenerFunction(event) with event.daoEvent as
+	 *            daoEvent.action
+	 *            daoEvent.objectType
+	 *            daoEvent.objectId
+	 *            daoEvent.data
+	 *            daoEvent.opts
+	 *            daoEvent.resultPromise 
+	 * 
+	 */
+	brite.dao.onDao = function(events,objectTypes,listenerFunction,namespace){
+		var map = buildDaoOnEventParamMap(arguments);
+		$daoDao.on(map.events,map.objectTypes,map.func);
+		return map.namespace;	
+	}
+	
+	brite.dao.offDao = function(namespace){
+		$daoDao.off("." + namespace);
+  }
+  
+  function _triggerOnDao(resultPromise,action,objectType,objectId,data,opts){
+		var daoEvent = {
+				action: action,
+				objectType: objectType,
+				objectId: objectId,
+				data: data, 
+				opts: opts,
+				resultPromise: resultPromise
+		}
+		
+		_triggerDaoEvent(onDaoReceiverDic,$daoDao,objectType,daoEvent);
+		
+		// trigger the old way for backward compatibility
+		callDaoListeners(resultPromise,action,objectType,objectId, data, opts)
+  }
+	// --------- /brite.dao.onDao --------- //
+	
+	// --------- brite.dao.onResult --------- //
+	var $daoResult = $("<div></div>");
+	// dictionary of {objectType:$dataEventReceiver} 
+	var onResultReceiverDic = {};
+	
+	
+	/**
+	 * This will trigger when the dao resolve the result of a particular DAO call. 
+	 * This will not trigger in case of a dao failure.
+	 *  
+	 * @param events            e.g., "create, list, get" (null for any)
+	 * @param objectTypes       e.g., "User, Task" (null for any)
+	 * @param listenerFunction  The function to be called with the daoEvent
+	 *            listenerFunction(daoEvent)
+	 *            daoEvent.action
+	 *            daoEvent.objectType
+	 *            daoEvent.objectId
+	 *            daoEvent.data
+	 *            daoEvent.opts
+	 *            daoEvent.result 
+	 */
+	brite.dao.onResult = function(events,objectTypes,listenerFunction,namespace){
+		var map = buildDaoOnEventParamMap(arguments);
+		$daoResult.on(map.events,map.objectTypes,map.func);
+		return map.namespace;
+  }
+  
+  brite.dao.offResult = function(namespace){
+  	$daoResult.off("." + namespace);
+  }  
+  
+ 	function _triggerOnResult(action,objectType,result){
+ 		var daoEvent = {
+			action: action,
+			result: result
+		};
+		
+		_triggerDaoEvent(onResultReceiverDic,$daoResult,objectType,daoEvent);
+ 	}  
+	// --------- /brite.dao.onResult --------- //
+
+	
+	// --------- Brite.dao.onDataChange --------- //
+	var $daoDataChange = $("<div></div>");
+	
+	// dictionary of {objectType:$dataEventReceiver} 
+	var dataChangeReceiverDic = {};
+	
+	/**
+	 * This trigger on data change event only (like "create, update, remove") and not on others. For other binding, 
+	 * use the brite.dao.onResult which will trigger anytime
+	 * 
+	 * @param {String} namespace: the namespace for this event. 
+	 * @param {String} events: this dao action names e.g., "create, update" (null to listen to all events)
+	 * @param {String} objectTypes: the object types e.g., "User, Task" (null for any object type);
+	 */
+	brite.dao.onDataChange = function(events,objectTypes, func, namespace){
+	 	var map = buildDaoOnEventParamMap(arguments);
+		$daoDataChange.on(map.events,map.objectTypes,map.func);
+		return map.namespace;
+ 	}
+ 	
+ 	brite.dao.offDataChange = function(namespace){
+ 		$daoDataChange.off("." + namespace);
+ 	}
+ 	
+ 	brite.dao.triggerDataChange = function(action,objectType,objectId,newData,saveData){
+ 		var daoEvent = {
+			action: action,
+			objectType: objectType,
+			objectId: objectId,
+			newData: newData,
+			saveData: saveData
+		};
+		
+		_triggerDaoEvent(dataChangeReceiverDic,$daoDataChange,objectType,daoEvent);
+ 	}
+ 	
+ 	
+ 	function _triggerDataChange(action,objectType,objectId,newData,saveData){
+ 		// the current way
+ 		brite.dao.triggerDataChange(action,objectType,objectId,newData,saveData);
+ 		
+ 		// call the legacy API
+ 		callDataChangeListeners(action,objectType, objectId, newData, saveData);
+ 	}
+ 	
+	// --------- /Brite.dao.onDataChange --------- //
+	
+  // --------- DAO Interface Callers --------- //
+	/**
+	 * Return the id property name (this is the only method in brite.dao that is not deferred)
+	 */
+	brite.dao.getIdName = function(objectType) {
+		return getDao(objectType).getIdName(objectType);
+	}
+
+	/**
+	 * Wrap the brite.sdm.get(objectType,id) with a deferred result
+	 */
+	brite.dao.get = function(objectType, objectId) {
+		var resultPromise = wrapWithDeferred(getDao(objectType).get(objectType, objectId));
+		
+		_triggerOnDao(resultPromise,"get",objectType,objectId);
+		
+		resultPromise.done(function(result){
+			_triggerOnResult("get",objectType,result);
+		});		
+		
+		return resultPromise;
+	}
+
+	/**
+	 * Wrap the brite.sdm.list(objectType,opts) with a deferred result
+	 */
+	brite.dao.list = function(objectType, opts) {
+		var resultPromise = wrapWithDeferred(getDao(objectType).list(objectType, opts));
+		
+		_triggerOnDao(resultPromise,"list",objectType,null,null,opts);
+		
+		resultPromise.done(function(result){
+			_triggerOnResult("list",objectType,result);
+		});		
+		
+		return resultPromise;		
+	}
+
+	/**
+	 * Wrap the brite.sdm.create(objectType,data) with a deferred result
+	 */
+	brite.dao.create = function(objectType, data) {
+		var resultPromise = wrapWithDeferred(getDao(objectType).create(objectType, data));
+		
+		_triggerOnDao(resultPromise,"create",objectType,null,data);
+		
+		resultPromise.done(function(result){
+			_triggerOnResult("create",objectType,result);
+			_triggerDataChange("create",objectType, null, result, data);
+		});
+		
+		return resultPromise;
+	}
+
+	/**
+	 * Wrap the brite.sdm.update(objectType,id,data) with a deferred result
+	 */
+	brite.dao.update = function(objectType, objectId, data) {
+		var resultPromise = wrapWithDeferred(getDao(objectType).update(objectType, objectId, data));
+		
+		_triggerOnDao(resultPromise,"update",objectType,objectId,data);
+		
+		resultPromise.done(function(result){
+			_triggerOnResult("update",objectType,result);
+			_triggerDataChange("update",objectType, objectId, result, data);
+		});
+		
+		return resultPromise; 
+	}
+
+	/**
+	 * Wrap the brite.sdm.remove(objectType,id) with a deferred result
+	 */
+	brite.dao.remove = function(objectType, objectId) {
+		var resultPromise = wrapWithDeferred(getDao(objectType).remove(objectType, objectId));
+		
+		_triggerOnDao(resultPromise,"remove",objectType,objectId);
+		
+		resultPromise.done(function(result){
+			_triggerOnResult("remove",objectType,objectId);
+			_triggerDataChange("remove",objectType, objectId);
+		});
+		
+		return resultPromise;
+	}
+	
+	/**
+	 * Wrap the brite.sdm.invoke(methodName,objectType) with a deferred result
+	 */
+	brite.dao.invoke = function(methodName, objectType) {
+		var args = Array.prototype.slice.call(arguments,0); 
+		
+		var dao = getDao(objectType);
+		if (!dao) throw  ("cannot find dao for " + objectType);
+		if (!dao[methodName]) throw  ("no custom method for " + methodName);
+			
+		var resultPromise = wrapWithDeferred(dao[methodName].apply(dao,args.slice(1)));
+		
+		// TODO: need to trigger with all the arguments pass in the invoke
+		_triggerOnDao(resultPromise,methodName,objectType);
+		
+		resultPromise.done(function(result){
+			_triggerOnResult(methodName,objectType,result);
+			
+			if (dao.isDataChange && dao.isDataChange(methodName)){
+				_triggerDataChange("methodName",objectType,result);
+			}
+			
+		});
+		
+		return resultPromise;		
+	}	
+	// --------- /DAO Interface Callers --------- //
+
+	/**
+	 * Wrap with a deferred object if the obj is not a deferred itself.
+	 */
+	function wrapWithDeferred(obj) {
+		//if it is a deferred, then, trust it, return it.
+		if (obj && $.isFunction(obj.promise)) {
+			return obj;
+		} else {
+			var dfd = $.Deferred();
+			dfd.resolve(obj);
+			return dfd;
+		}
+	}
+
+  // ------- Legacy Event Management --------- //
+  
 	
 	/**
 	 * 
@@ -79,6 +448,7 @@ brite.dao = {};
 	
 	
 	/**
+	 * LEGACY: do not use. Use, brite.dao.onDataChange
 	 * Add a change listener function for an objectType. <br />
 	 * Data Change listener get triggered on create, update, and remove.
 	 * 
@@ -101,202 +471,20 @@ brite.dao = {};
 		return bindingId;
 	};
 	
+	/**
+	 * LEGACY: do not use. User, brite.dao.offDataChange
+	 */
 	brite.removeDataChangeListener = function(listenerId){
 		$.each(daoChangeEventListeners,function(idx,listeners){
 			delete listeners[listenerId];
 		})
-	}
-
-	/**
-	 * Return the id property name (this is the only method in brite.ddm that is not deferred)
-	 */
-	brite.dao.getIdName = function(objectType) {
-		return brite.sdm.getIdName(objectType);
-	}
-
-	/**
-	 * Wrap the brite.sdm.get(objectType,id) with a deferred result
-	 */
-	brite.dao.get = function(objectType, id) {
-		return wrapWithDeferred(brite.sdm.get(objectType, id));
-	}
-
-	/**
-	 * Wrap the brite.sdm.list(objectType,opts) with a deferred result
-	 */
-	brite.dao.list = function(objectType, opts) {
-		return wrapWithDeferred(brite.sdm.list(objectType, opts));
-	}
-
-	/**
-	 * Wrap the brite.sdm.create(objectType,data) with a deferred result
-	 */
-	brite.dao.create = function(objectType, data) {
-		return wrapWithDeferred(brite.sdm.create(objectType, data));
-	}
-
-	/**
-	 * Wrap the brite.sdm.update(objectType,id,data) with a deferred result
-	 */
-	brite.dao.update = function(objectType, id, data) {
-		return wrapWithDeferred(brite.sdm.update(objectType, id, data));
-	}
-
-	/**
-	 * Wrap the brite.sdm.remove(objectType,id) with a deferred result
-	 */
-	brite.dao.remove = function(objectType, id) {
-		return wrapWithDeferred(brite.sdm.remove(objectType, id));
-	}
-	
-	/**
-	 * Wrap the brite.sdm.invoke(methodName,objectType) with a deferred result
-	 */
-	brite.dao.invoke = function(methodName, objectType) {
-		var args = Array.prototype.slice.call(arguments,0); 
-		return wrapWithDeferred(brite.sdm.invoke.apply(null,args));
-	}	
-
-	// ------- brite.sdm DAO API ------ //
-	/**
-	 * @namespace brite.sdm Straight Data Manager API that just return what the DAO returns (it does not Deferred wrap). This is mostly used by the brite.dao, but
-	 * could be used by the application when it is ok to have blocking data calls (i.e. local data) or to expose the async/sync to the application layer.
-	 */
-	brite.sdm = {};
-
-	/**
-	 * DAO Interface: Return the property ID name
-	 * @param {string} the objectType
-	 * @return the id (this is not deferred)
-	 * @throws error if dao cannot be found
-	 */
-	brite.sdm.getIdName = function(objectType) {
-		return getDao(objectType).getIdName(objectType);
-	}
-
-	/**
-	 * DAO Interface: Return a value or deferred object (depending of DAO impl) for this objectType and id.
-	 * @param {Object} objectType
-	 * @param {Object} id
-	 * @return
-	 */
-	brite.sdm.get = function(objectType, id) {
-		var result = getDao(objectType).get(objectType, id);
-		
-		callDaoListeners(result,"get",objectType,id);
-		
-		return result;
-	};
-
-	/**
-	 * DAO Interface: Return an array of values or a deferred object (depending of DAO impl) for this objectType and options
-	 * @param {Object} objectType
-	 * @param {Object} opts (not supported yet)
-	 *           opts.pageIndex {Number} Index of the page, starting at 0.
-	 *           opts.pageSize  {Number} Size of the page
-	 *           opts.match     {Object}
-	 *           opts.orderBy   {String}
-	 *           opts.orderType {String} "asc" or "desc"
-	 */
-	brite.sdm.list = function(objectType, opts) {
-		var result =  getDao(objectType).list(objectType, opts);
-		
-		callDaoListeners(result,"list",objectType,null,null,opts);
-		
-		return result;
-	};
-
-	/**
-	 * DAO Interface: Create a new instance of the object for a give objectType and data. <br />
-	 *
-	 * The DAO should return or resolve with the newly created data.
-	 *
-	 * @param {Object} objectType
-	 * @param {Object} data
-	 */
-	brite.sdm.create = function(objectType, data) {
-		var result = getDao(objectType).create(objectType, data);
-
-		callDaoListeners(result,"create",objectType,null,data);
-		
-		// if the result is a deferred object, then, wait until done to callDataChangeListeners
-		if (result && $.isFunction(result.promise)) {
-			result.done( function(newData) {
-				// TODO: need to get the id to set the id rather than null
-				callDataChangeListeners("create",objectType, null, newData, data);
-			});
-
-		} else {
-			callDataChangeListeners("create",objectType, null, result, data); 
-		}
-		return result;
-
-	};
-
-	/**
-	 * DAO Interface: update a existing id with a set of property/value data.
-	 *
-	 */
-	brite.sdm.update = function(objectType, id, data) {
-		var result = getDao(objectType).update(objectType,id, data);
-
-		callDaoListeners(result,"update",objectType,id,data);
-		
-		// if the result is a deferred object, then, wait until done to callChangeListeners
-		if (result && $.isFunction(result.promise)) {
-			result.done( function(newData) {
-				callDataChangeListeners("update",objectType, id, newData, data); 
-			});
-
-		} else {
-			callDataChangeListeners("update",objectType, id, result, data); 
-
-		}
-		return result;
-
-	};
-
-	/**
-	 * DAO Interface: remove an entity for a given type and id.
-	 *
-	 */
-	brite.sdm.remove = function(objectType, id) {
-		var result = getDao(objectType).remove(objectType, id);
-
-		callDaoListeners(result,"remove",objectType,id);
-		
-		// if the result is a deferred object, then, wait until done to callChangeListeners
-		if (result && $.isFunction(result.promise)) {
-			result.done( function(removedObject) {
-				callDataChangeListeners("remove",objectType, id, null, null); 
-			});
-
-		} else {
-			callDataChangeListeners("remove",objectType, id, null, null);
-		}
-
-	};
-	
-	brite.sdm.invoke = function(methodName,objectType){
-		var args = Array.prototype.slice.call(arguments); 
-		
-		var dao = getDao(objectType);
-		if (!dao) throw  ("cannot find dao for " + objectType);
-		if (!dao[methodName]) throw  ("no custom method for " + method);
-		var result = dao[methodName].apply(dao,args.slice(1));
-		
-		callDaoListeners(result,methodName,objectType);
-		
-		return result;
-		
-	}
-
-	// ------- /brite.sdm DAO API ------ //
-
+	}  
+  
 	function callDaoListeners(result,action,objectType,id, data, opts){
 		var daoEvent = {
 				result: result,
 				action: action,
+				type: action,
 				objectType: objectType,
 				id: id,
 				data: data, 
@@ -322,14 +510,15 @@ brite.dao = {};
 
 		var dataChangeEvent = {
 			action: action,
+			type: action,
 			objectType: objectType,
-			id: id,
+			objectId: id,
 			newData: newData,
 			saveData: saveData
 		};
 		
-		_callDataChangeListeners(dataChangeEvent)
-
+		// old way: addDataChangeListener
+		_callDataChangeListeners(dataChangeEvent);
 	};
 	
 	function _callDataChangeListeners(dataChangeEvent){
@@ -342,24 +531,7 @@ brite.dao = {};
 		}		
 	}
 	
-	brite.triggerDataChange = function(dataChangeEvent){
-		_callDataChangeListeners(dataChangeEvent);
-	}
-	
-
-	/**
-	 * Wrap with a deferred object if the obj is not a deferred itself.
-	 */
-	function wrapWithDeferred(obj) {
-		//if it is a deferred, then, trust it, return it.
-		if (obj && $.isFunction(obj.promise)) {
-			return obj;
-		} else {
-			var dfd = $.Deferred();
-			dfd.resolve(obj);
-			return dfd;
-		}
-	}
+  // ------- Legacy Event Management --------- //	
 
 })(jQuery);
 
