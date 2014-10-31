6d7f50e7e283f182c8055335448b29ca4526fac6
jeremychone <jeremy.chone@gmail.com>
2014-10-30 20:15:44 -0700

    . update the brite.js and brite.min.js to match the latest 1.1.3 release

--- dist/brite.js
+++ dist/brite.js
@@ -1,6 +1,6 @@
 var brite = brite || {};
 
-brite.version = "1.1.2";
+brite.version = "1.1.3";
 
 if ( typeof module === "object" && module && typeof module.exports === "object" ) {
 		module.exports = brite;
